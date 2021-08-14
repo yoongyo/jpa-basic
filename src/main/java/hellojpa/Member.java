be81dc4aaede05666423cc3b38e96bcd5b363cae
unknown <jyg0172@naver.com>
2021-08-14 23:56:05 +0900

    clear

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -10,13 +10,18 @@ public class Member {
     private Long id;
     @Column(name = "name")
     private String username;
+
     private Integer age;
+
     @Enumerated(EnumType.STRING)
     private RoleType roleType;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
+    
     @Lob
     private String description;
 
