337cd1750c3705fe6433aafe58154a9f435acf49
unknown <jyg0172@naver.com>
2021-08-14 23:55:09 +0900

    role type

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -23,26 +23,4 @@ public class Member {
     public Member() {
 
     }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
